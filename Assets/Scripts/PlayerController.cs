4c4ba6d09c184afe443a2832f8a62b742a0b5e03
Jim Magnusson <jim.magnusson97@gmail.com>
2022-10-01 14:10:14 +0200

    Updated player movement

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -14,13 +14,15 @@ public class PlayerController : MonoBehaviour
 
     [SerializeField] private float walkingThreshold = 0.5f;
     
-    [SerializeField] private float wallCheckDistance = 1f;
-
     //private Health health;
     private Animator animator;
     private MovementType movementType = MovementType.idle;
 
     private PlayerControls playerControls;
+    
+    private CharacterController controller;
+
+    private Quaternion targetRotation;
     
     private void Awake() {
         playerControls = new PlayerControls();
@@ -38,39 +40,30 @@ public class PlayerController : MonoBehaviour
     {
         //health = GetComponent<Health>();
         animator = GetComponent<Animator>();
+        
+        controller = GetComponent<CharacterController>();
     }
     // Update is called once per frame
     void Update()
     {
-        Vector2 move = playerControls.Standard.Move.ReadValue<Vector2>();
+        Vector2 input = playerControls.Standard.Move.ReadValue<Vector2>();
+
+        Vector3 move = new Vector3(input.x, 0, input.y);
+        
+        controller.Move(move * Time.deltaTime * moveSpeed);
         
         // Rotate player
-        if(move.magnitude > 0)
+        if(input.magnitude > 0)
         {
             //Adding these vectors together will result in a position in the world, that is around your player.
-            Vector3 goal = new Vector3(move.x, 0, move.y) + transform.position;
+            Vector3 goal = move + transform.position;
 
             //Now we create a target rotation, by creating a direction vector: (This would be just be inputVector in this case).
-            Quaternion targetRotation = Quaternion.LookRotation(goal - transform.position);
-
+            targetRotation = Quaternion.LookRotation(goal - transform.position);
+            
             //Rotate smoothly to this target:
-            transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, rotationSpeed);
+            transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, Time.deltaTime * rotationSpeed);
         }
-        
-        RaycastHit hit;
 
-        // Does the ray intersect any objects excluding the player layer
-        if (Physics.Raycast(transform.position, transform.TransformDirection(Vector3.forward), out hit, wallCheckDistance))
-        {
-            Debug.DrawRay(transform.position, transform.TransformDirection(Vector3.forward) * hit.distance, Color.yellow);
-        }
-        else
-        {
-            transform.position += new Vector3(move.x * moveSpeed * Time.deltaTime, 0, move.y * moveSpeed * Time.deltaTime);
-
-        }
-        
-        
-        //Debug.Log(move);
     }
 }
