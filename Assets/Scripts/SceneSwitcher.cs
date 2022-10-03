107eac1eef2cfaea6e771132e411eab901eb0645
Jim Magnusson <jim.magnusson97@gmail.com>
2022-10-03 19:01:48 +0200

    Fixed render texture pixels activation

--- Assets/Scripts/SceneSwitcher.cs
+++ Assets/Scripts/SceneSwitcher.cs
@@ -1,7 +1,9 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using Cinemachine;
 using UnityEngine;
+using UnityEngine.UI;
 
 public enum SceneState
 {
@@ -16,7 +18,17 @@ public class SceneSwitcher : MonoBehaviour
     [SerializeField] private GameObject chiptuneScene;
     
     [SerializeField] private float sceneTime = 10f;
+
+    [SerializeField] private Camera mainCamera;
     
+    [SerializeField] private Camera extraCamera;
+    
+    [SerializeField] private Camera someCamera;
+    [SerializeField] private CinemachineVirtualCamera vCam;
+    [SerializeField] private CinemachineVirtualCamera vCamArcade;
+    [SerializeField] private RawImage pixelImage;
+    
+    //[SerializeField] private RenderTexture pixelRenderTexture;
     
     private SceneState currentSceneState;
     private float sceneTimer;
@@ -53,10 +65,22 @@ public class SceneSwitcher : MonoBehaviour
             case SceneState.club:
                 currentScene = chiptuneScene;
                 currentSceneState = SceneState.chiptune;
+                mainCamera.gameObject.SetActive(false);
+                extraCamera.gameObject.SetActive(true);
+                someCamera.gameObject.SetActive(true);
+                pixelImage.enabled = true;
+                vCam.gameObject.SetActive(false);
+                vCamArcade.gameObject.SetActive(true);
                 break;
             case SceneState.chiptune:
                 currentScene = rockScene;
                 currentSceneState = SceneState.rock;
+                mainCamera.gameObject.SetActive(true);
+                extraCamera.gameObject.SetActive(false);
+                someCamera.gameObject.SetActive(false);
+                pixelImage.enabled = false;
+                vCam.gameObject.SetActive(true);
+                vCamArcade.gameObject.SetActive(false);
                 break;
         }
         if (OnSwitchSceneEvent != null)
